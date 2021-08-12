f863e34866fe3e3ea7f071a5d5792b86e65e0093
Senimtra <senimtra@hotmail.com>
2021-08-12 16:20:02 +0200

    solve iteration 3: get(pos)

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ class SortedList {
     this.length = this.items.length;
   }
 
-  get(pos) { }
+  // ###########################
+  // ## Iteration 3: get(pos) ##
+  // ###########################
+
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
   max() { }
 
